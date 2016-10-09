1fd68a9f829e2715030055d2b08103b7d80245f2
Vasilis Sakkas <billsakkas@me.com>
2016-10-09 20:57:32 +0300

    First Commit of the website

---
+++ js/myApp.js
@@ -0,0 +1,73 @@
+(function () {
+'use strict';
+
+angular.module('myApp', [])
+.controller('myController', myController)
+.service('getItemsService', getItemsService)
+.directive('foundItems', FoundItemsDirective)
+.constant('link', 'https://github.com/billsakkas/Website/blob/master/json/')
+
+myController.$inject = ['getItemsService'];
+function myController(getItemsService) {
+  var ctrl = this;
+
+  ctrl.getItems = function (jsonFile) {
+    console.log("Clicked! :D");
+    ctrl.recieved = undefined;
+    getItemsService.getItems(jsonFile)
+    .then(function (result) {
+      ctrl.recieved = result;
+      console.log(ctrl.recieved);
+    })
+    .catch(function(error) {
+      console.log("Something went wrong!!! Error code = " + error);
+    });
+
+  }
+  ctrl.notRecieved = function () {
+    if (ctrl.recieved == undefined) {
+      return false;
+    }
+    return ctrl.recieved.length == 0;
+  }
+
+
+}
+
+
+getItemsService.$inject = ['$http'];
+function getItemsService($http) {
+  var service = this;
+  service.getItems = function(jsonFile) {
+    return $http({
+      method: "GET",
+      // url: (link + jsonFile)
+      url: ("https://raw.githubusercontent.com/billsakkas/Website/master/json/Collection1.json")
+    }).then(function(response){
+      console.log(response.data.links[0]);
+      return response.data.links
+    });
+  }
+}
+
+
+function FoundItemsDirective() {
+  var ddo = {
+        restrict: '=',
+        template: 'Hello',
+        scope: {
+            items: '<recievedItems'
+        },
+        controller: DirectiveController,
+        controllerAs: 'dirCtrl',
+        bindToController: true,
+        transclude: true
+        };
+    return ddo;
+  };
+
+
+function DirectiveController() {
+
+}
+})();
